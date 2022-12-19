5356978bfc3883bb57ed1719803d100a87bdf7ba
coenen <coenen@aliyun.com>
2022-12-19 14:46:01 +0800

    删除引用

--- LTMSwift/Classes/UIButton+Extension.swift
+++ LTMSwift/Classes/UIButton+Extension.swift
@@ -7,7 +7,6 @@
 
 import Foundation
 import UIKit
-import Kingfisher
 
 fileprivate var rectNameKey:(Character?,Character?,Character?,Character?)
 
--- LTMSwift/Classes/UIImage+Extension.swift
+++ LTMSwift/Classes/UIImage+Extension.swift
@@ -6,7 +6,6 @@
 //
 
 import Foundation
-import Kingfisher
 
 public extension UIImage{
     /**
